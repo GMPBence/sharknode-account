57258f0c4e56782956e15f15f8f4d27a8dcdafea
kbence0429 <kovacs.bence0429@gmail.com>
2025-11-30 19:25:25 +0100

    minor layout changes

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,10 +1,31 @@
-import { Outlet, useLocation } from 'react-router'
+import { Outlet, useLocation, useNavigate } from 'react-router'
 import './main-layout.scss'
 import Sidebar from '../components/sidebar/Sidebar'
 import PopupElement from '../popup/PopupElement'
+import { Obj, useAuth } from 'magicauth-client'
+
+import logout from '../assets/img/logout.png'
 
 const MainLayout = () => {
+    const magic = useAuth()
     const location = useLocation()
+    const navigate = useNavigate()
+
+    const titles: Obj<string> = {
+
+        '/': '',
+        '/services': 'Szolgáltatások',
+        '/yourservices': 'Szervereid',
+        '/manage': 'Szervereid > Kezelés',
+        '/settings': 'Beállítások'
+
+    }
+    const title = titles[ location.pathname ]
+
+    const logoutClick = () => {
+        magic.auth().logout()
+        navigate('/')
+    }
     
     return (
         <div className='main-layout'>
@@ -13,13 +34,14 @@ const MainLayout = () => {
             </div>
             <div className="main-layout_container">
                 <div className="main-layout_topbar">
-                    <h1>Szolgaltatasok</h1>
+                    <h1>{ title }</h1>
                     <div className="main-layout_topbar_profile">
                         <div className="text">
                             <h6>PROFIL</h6>
                             <h5>kbence0429</h5>
                         </div>
                         <div className="image"></div>
+                        <img src={ logout } alt="logout" className='logout' onClick={ logoutClick } />
                     </div>
                 </div>
                 <div className="main-layout_content" key={ location.pathname }>
